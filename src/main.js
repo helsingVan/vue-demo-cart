da3029999f349bad66e39a5eff3ba559e9890bef
helsingvan <it.fan@foxmail.com>
2017-07-04 14:46:39 +0800

    pack

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import 'common/stylus/index.styl'
+// import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
