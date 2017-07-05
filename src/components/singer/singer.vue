96ef45c166292998c7bd5d097e26fbf4838ef949
helsingvan <it.fan@foxmail.com>
2017-07-05 17:59:55 +0800

    singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,88 @@
 <template>
-  <div>singer</div>
+  <div class="singer" ref="singer">
+<!--     <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+    <router-view></router-view> -->
+  </div>
 </template>
 
 <script>
-	
+import { getSingerList } from 'api/singer';
+import { ERR_OK } from 'api/config';
+import { Singer } from 'common/js/singer';
+
+const hotTitle = '热门';
+const hotSingerLen = 10;
+export default{
+  name: 'singer',
+  data() {
+  	return {
+  	  data: []
+  	}
+  },
+  created() {
+  	this._getSingerList();
+  },
+  methods: {
+  	_getSingerList() {
+  	  const self = this;
+  	  getSingerList().then((res)=>{
+  	  	if(res.code === ERR_OK) {
+  	  	  self.data = self._normalizeSinger(res.data.list);
+  	  	  console.log(self.data);
+  	  	}
+  	  });
+  	},
+  	_normalizeSinger(list) {
+  	  let map = {
+  	  	hot: {
+  	  	  title: hotTitle,
+  	  	  item: []
+  	  	}
+  	  };
+  	  list.forEach((v,i)=> {
+  	  	if(i < 10) {
+  	  	  map.hot.item.push(new Singer({
+  	  	  	id: v.Fsinger_mid,
+  	  	  	name: v.Fsinger_name
+  	  	  }));
+  	  	}
+  	  	const key = v.Findex;
+  	  	if(!map[key]) {
+  	  	  map[key] = {
+  	  	  	title: key,
+  	  	  	item: []
+  	  	  };
+  	  	}
+  	  	map[key].item.push(new Singer({
+  	  		id: v.Fsinger_mid,
+  	  		name: v.Fsinger_name
+  	  	}));
+  	  });
+  	  
+  	  // 为得到有序列表，处理map
+  	  let hot = [];
+  	  let ret = [];
+  	  for(let key in map) {
+  	  	let val = map[key];
+  	  	if(val.title.match(/[a-zA-Z]/)) {
+  	  	  ret.push(val);
+  	  	}else if(val.title === hotTitle) {
+  	  	  hot.push(val);
+  	  	}
+  	  }
+  	  ret.sort((a,b)=> {
+  	  	return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+  	  });
+  	  return hot.concat(ret);
+  	}
+  }
+}
 </script>
 
-<style>
-	
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: 'recommend'
+      redirect: 'singer'
     },
     {
       path: '/rank',
