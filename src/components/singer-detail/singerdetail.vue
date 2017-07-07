86404ecb2cad65e362389306883816ce5ce5e8d0
helsingvan <it.fan@foxmail.com>
2017-07-07 14:57:37 +0800

    dot

--- src/components/singer-detail/singerdetail.vue
+++ src/components/singer-detail/singerdetail.vue
@@ -45,16 +45,15 @@ export default {
   	  })
   	},
   	_normalizeSongs(list) {
-        let ret = []
+        let ret = [];
         list.forEach((item) => {
-          let {musicData} = item
+          let {musicData} = item;
           if (musicData.songid && musicData.albummid) {
-            ret.push(createSong(musicData))
+            ret.push(createSong(musicData));
           }
-        })
-        return ret
+        });
+        return ret;
     }
-    
   }
 }
 </script>
