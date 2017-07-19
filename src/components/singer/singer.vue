bf19b3ed4c7cc06c2e82eeca721f22d8bfeae57f
helsingvan <it.fan@foxmail.com>
2017-07-19 18:04:24 +0800

    test

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="singer" ref="singer">
+  <div class="singer" ref="singer" id="yes">
     <list-view :data="singers" ref="list" @select="emitSelect"></list-view>
     <router-view></router-view>
   </div>
