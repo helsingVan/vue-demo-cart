76defc53a4c489388b01a0f31bef710e70dd51db
helsingvan <it.fan@foxmail.com>
2017-09-08 16:23:03 +0800

    test

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import jsonp from 'common/js/jsonp';
 import { commonParams,options } from './config';
 
-console.log(2);
+console.log(3);
 function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
 
