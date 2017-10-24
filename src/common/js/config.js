d46da181887661c5d233478f177bf24646f52db7
helsingvan <it.fan@foxmail.com>
2017-10-24 14:20:57 +0800

    reset

--- src/common/js/config.js
+++
@@ -1,7 +0,0 @@
-const playMode = {
-  sequence: 0,
-  loop: 1,
-  random: 2
-};
-
-export {playMode};
--- src/common/js/singer.js
+++
@@ -1,9 +0,0 @@
-class Singer {
-  constructor({id,name}) {
-  	this.id = id;
-  	this.name = name;
-  	this.avatar = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${id}.jpg?max_age=2592000`;
-  }
-}
-
-export { Singer };
